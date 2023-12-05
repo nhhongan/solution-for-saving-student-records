a3511e1fc406798807861d129e902d898144403e
Phu Chau <chauanphu@gmail.com>
2023-12-05 16:22:30 +0700

    Finish CourseProgramPage template

--- frontend/src/components/Table/Table.tsx
+++ frontend/src/components/Table/Table.tsx
@@ -36,32 +36,34 @@ type TableProps = {
 
 const Table: React.FC<TableProps> = ({headers, contents, type}) => {  
   return (
-    <table className={`table ${type}`} cellSpacing={0}>
-      <thead>
-        <tr>
-          <th></th>
-          {headers.map((header, index) => (
-            <th key={index}>{header}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {contents?.map((row, index) => (
-          <tr key={index} className={row.type}>
-            {row.type !== RowType.Aggregate && <td>{index + 1}</td>}
-            {row.cols.map((col, index) => ( 
-              <td 
-                className={col.name && "course"} 
-                key={index} 
-                rowSpan={col.rowSpan}
-                colSpan={col.colSpan}>
-                {col.name}
-              </td>
+    <div className="table-wrapper">
+      <table className={`table ${type}`} cellSpacing={0}>
+        <thead>
+          <tr>
+            <th></th>
+            {headers.map((header, index) => (
+              <th key={index}>{header}</th>
             ))}
-        </tr>
-        ))}
-      </tbody>
-    </table>
+          </tr>
+        </thead>
+        <tbody>
+          {contents?.map((row, index) => (
+            <tr key={index} className={row.type}>
+              {row.type !== RowType.Aggregate && <td>{index + 1}</td>}
+              {row.cols.map((col, index) => ( 
+                <td 
+                  className={col.name && "course"} 
+                  key={index} 
+                  rowSpan={col.rowSpan}
+                  colSpan={col.colSpan}>
+                  {col.name}
+                </td>
+              ))}
+          </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
--- frontend/src/pages/CourseProgramPage/CourseProgramPage.tsx
+++ frontend/src/pages/CourseProgramPage/CourseProgramPage.tsx
@@ -1,9 +1,25 @@
+import Table, { Row, TableType } from 'components/Table/Table';
 import React from 'react';
 
+const headers = ["Course ID", "Course Name", "Credits", "Semester", "Elective", "Learnt"]
 const CourseProgramPage: React.FC = () => {
+  const program = 'Data Science';
+  const school_year = '2022-2026';
+  const contents: Row[] = []
+  for (let i = 0; i < 3; i++) {
+    const row = new Row();
+    row.cols.push({name: 'IT135IU', rowSpan: 1});
+    row.cols.push({name: 'Probability', rowSpan: 1});
+    row.cols.push({name: '3', rowSpan: 1});
+    row.cols.push({name: '1', rowSpan: 1});
+    row.cols.push({name: 'Yes', rowSpan: 1});
+    row.cols.push({name: 'Yes', rowSpan: 1});
+    contents.push(row);
+  }
   return (
-    <div>
-      {/* Your content goes here */}
+    <div className='page'>
+      <h2 className='title'>{program} {school_year}</h2>
+      <Table headers={headers} type={TableType.Type1} contents={contents}/>
     </div>
   );
 };
