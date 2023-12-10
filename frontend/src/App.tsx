68f67d1faf4755f92a48d8f33dfb274d4002bac9
Phu Chau <chauanphu@gmail.com>
2023-12-10 07:13:51 +0700

    Add redirect on unauthenticated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import './assets/css/App.scss';
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes, Navigate, useLocation, Outlet} from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage';
 import CourseRegisPage from './pages/CourseRegisPage/CourseRegisPage';
 import TimeTablePage from 'pages/TimeTablePage/TimeTablePage';
@@ -11,11 +11,19 @@ import Footer from 'layouts/Footer/Footer';
 import FeePage from 'pages/FeePage/FeePage';
 import CourseProgramPage from 'pages/CourseProgramPage/CourseProgramPage';
 
+export const ProtectedRoute = () => {
+  const location = useLocation();
+  const isAuthenticated = localStorage.getItem('token') !== null;
+  return isAuthenticated
+    ? <RootLayout/>
+    : <Navigate to="/login" state={{ from: location }} replace />;
+};
+
 function App() {
   return (
     <BrowserRouter>
         <Routes>
-          <Route path='/' element={<RootLayout/>}>
+          <Route path='/' element={<ProtectedRoute/>}>
             <Route index element={<HomePage />}/>
             <Route path='/course-registration' element={<CourseRegisPage />}/>
             <Route path='/timetable' element={<TimeTablePage />}/>
--- frontend/src/pages/Login/LoginPage.tsx
+++ frontend/src/pages/Login/LoginPage.tsx
@@ -1,69 +1,77 @@
-import React, { useState, useContext } from 'react';
-import './LoginPage.css';
-import api from "../../api.js";
-import { UserContext } from "../../context/UserContext";
-
+import React, { useState, useContext } from "react";
+import "./LoginPage.css";
+import axios from "axios";
 
 const LoginPage: React.FC = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState("");
-    const [, setToken] = useContext(UserContext);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUsername(event.target.value);
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const submitLogin = async () => {
+    const user = {
+      username: username,
+      password: password,
     };
-
-    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(event.target.value);
-    };
-
-    const submitLogin = async () => {
-        const requestOptions = {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: JSON.stringify(
-            `grant_type=&username=${username}&password=${password}&scope=&client_id=&client_secret=`
-          ),
-        };
-    
-        const response = await fetch("/auth/token", requestOptions);
-        const data = await response.json();
-    
-        if (!response.ok) {
-          setErrorMessage(data.detail);
-        } else {
-          setToken(data.access_token);
+    await axios
+      .post(process.env.API_ENDPOINt+"/token" as string, user)
+      .then((res) => {
+        if (res.status === 200) {
+          localStorage.setItem("token", res.data.token);
         }
-    };
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        submitLogin();
-    };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    submitLogin();
+  };
 
-    return (
-        <main id='login-container'>
-            <div className="login">
-                <div className='logo'><h3>UniLife</h3></div>
-                <div className="text">
-                    <h2>Welcome,</h2>
-                    <h3>Log in to continue!</h3>
-                </div>
-                <form onSubmit={handleSubmit}>
-                    <div className='form-group' id='form-username'>
-                        <label htmlFor="username">Username:</label>
-                        <input type="text" id="username" value={username} onChange={handleUsernameChange} />
-                    </div>
-                    <div className='form-group' id='form-password'>
-                        <label htmlFor="password">Password:</label>
-                        <input type="password" id="password" value={password} onChange={handlePasswordChange} />
-                    </div>
-                    <button id='submit' type="submit">Login</button>
-                </form>
-            </div>
-        </main>
-    );
+  return (
+    <main id="login-container">
+      <div className="login">
+        <div className="logo">
+          <h3>UniLife</h3>
+        </div>
+        <div className="text">
+          <h2>Welcome,</h2>
+          <h3>Log in to continue!</h3>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group" id="form-username">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </div>
+          <div className="form-group" id="form-password">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <button id="submit" type="submit">
+            Login
+          </button>
+        </form>
+      </div>
+    </main>
+  );
 };
 
 export default LoginPage;
