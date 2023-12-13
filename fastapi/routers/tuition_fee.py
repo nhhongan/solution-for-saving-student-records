b2b921357829aeff01bd808071e794ccc6c6c5a0
nhhongan <nhhongan@gmail.com>
2023-12-13 18:15:30 +0700

    Add tuition-fee router

--- fastapi/routers/tuition_fee.py
+++ fastapi/routers/tuition_fee.py
@@ -5,36 +5,34 @@ from sqlalchemy import and_
 from database.session import get_db
 from models.classes import Class
 from models.enrollment import Enrollment
+from models.course import Course
 from pydantic import BaseModel
 
 
 router = APIRouter(
-    prefix="/timetable", 
-    tags=["timetable"]
+    prefix="/tuition-fee", 
+    tags=["tuition-fee"]
 )
 
-class Timetable(BaseModel):
-    class_id: str
+class TuitionFee(BaseModel):
+    cid: str
+    class_id: int
     cname: str
     credits: int
-    day: str
-    room: str
-    start_period: int
-    end_period: int
-    start_date: str
-    end_date: str
-    pname: str
     semester: str
+    fee: int
+    total_fee: int
 
     class Config:
         orm_mode = True
 
 
-@router.get('/{sid}', response_model=List[Timetable])
-async def get_classes_of_student(sid: str, semester: str,db: Session = Depends(get_db)):
+
+@router.get('/{sid}', response_model=List[TuitionFee])
+async def get_classes_of_student(sid: str, semester: str, db: Session = Depends(get_db)):
     query_result = (
         db.query(Class, Enrollment)
-        .join(Enrollment, and_(Class.class_id == Enrollment.class_id, Enrollment.sid == sid))
+        .join(Enrollment, and_(Enrollment.class_id == Class.class_id, Enrollment.sid == sid))
         .filter(Class.semester == semester)
         .all()
     )
@@ -43,21 +41,22 @@ async def get_classes_of_student(sid: str, semester: str,db: Session = Depends(g
         raise HTTPException(status_code=404, detail=f"No classes found for semester {semester} for student {sid}")
 
     response_classes = []
+    total_fee = 0
+
     for class_, enrollment in query_result:
         credits = class_.course.credit if class_.course else "N/A"
-        timetable_entry = Timetable(
+        total_fee += class_.course.fee if class_.course else 0
+
+        tuition_fee_entry = TuitionFee(
+            cid=class_.course.cid,
             class_id=enrollment.class_id,
             cname=class_.cname,
             credits=credits,
-            day=class_.day,
-            room=class_.room,
-            start_period=class_.start_period,
-            end_period=class_.end_period,
-            start_date=class_.start_date,
-            end_date=class_.end_date,
-            pname=class_.professor_name,
             semester=class_.semester,
+            fee=class_.course.fee if class_.course else 0,
+            total_fee=total_fee,
         )
-        response_classes.append(timetable_entry)
+
+        response_classes.append(tuition_fee_entry)
 
     return response_classes
