6bcdb1ed13c944808751436449a7cd78d06bcd5b
nhhongan <nhhongan@gmail.com>
2023-12-26 20:40:26 +0700

    modify user table

--- backend/models/users.py
+++ backend/models/users.py
@@ -4,7 +4,6 @@ from database.__init__ import Base
 class User(Base):
     __tablename__ = 'user'
 
-    user_id = Column(Integer, primary_key=True, index=True)
     username = Column(VARCHAR(20), nullable=False)
     password = Column(String)
-    sid = Column(VARCHAR, ForeignKey('student.sid'))
+    sid = Column(VARCHAR, ForeignKey('student.sid'), primary_key=True)
